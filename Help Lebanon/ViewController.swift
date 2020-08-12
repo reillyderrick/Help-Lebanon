12aaa3ff57c36e8a1c2363e657857201599f9616
Reily Derrick <rderrick22@siprep.org>
2020-08-12 14:30:58 -0700

    hi

--- Help Lebanon/ViewController.swift
+++ Help Lebanon/ViewController.swift
@@ -9,28 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var homeButton1: UIButton!
-    @IBOutlet weak var readButton1: UIButton!
-    @IBOutlet weak var spreadButton1: UIButton!
-    var dropState1 = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    @IBAction func drop1(_ sender: Any) {
-        if (dropState1 == 0) {
-            homeButton1.isHidden = false
-            readButton1.isHidden = false
-            spreadButton1.isHidden = false
-            dropState1 = 1
-        } else if (dropState1 == 1){
-            homeButton1.isHidden = true
-            readButton1.isHidden = true
-            spreadButton1.isHidden = true
-            dropState1 = 0
-        }
     }
 }
 
