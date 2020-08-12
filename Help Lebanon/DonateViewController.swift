2c2793c6e82647bc7840f5e79a0686cf10d69683
maddieshenkan <maddieshenkan@gmail.com>
2020-08-12 16:37:34 -0700

    donate page + segue from main page to donate page

---
+++ Help Lebanon/DonateViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DonateViewController.swift
+//  Help Lebanon
+//
+//  Created by Maddie Shenkan on 8/12/20.
+//  Copyright © 2020 reilly. All rights reserved.
+//
+
+import UIKit
+
+class DonateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
